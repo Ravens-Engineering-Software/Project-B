135eb9581202e73fe9942611034e6a798ba0a880
Jacob Larose <stonejake1205@gmail.com>
2015-06-08 11:28:25 -0400

    ported for cryptography

--- Project B/src/FileManager.java
+++ Project B/src/FileManager.java
@@ -12,79 +12,42 @@ import java.util.ArrayList;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Jacob Larose
  */
 public class FileManager {
-    //Create variables for file management
-    String fileSeperator = System.getProperty("file.separator"); 
-    String relativePath = "src" + fileSeperator + "resorces" + fileSeperator + "incomesList.txt"; 
-    File income = new File(relativePath);
-    //Create variables for file reading
-    FileInputStream fis = null;
-    BufferedInputStream bis = null;
-    DataInputStream dis = null;
-    ArrayList<Double> incomeValue= new ArrayList<>();
-    ArrayList<Integer> incomeFrequency =new ArrayList<>();
-    ArrayList<String> incomeSource= new ArrayList<>();
-    StringBuilder encryptedStringBuilder = new StringBuilder();
-    
+
+    ArrayList<Double> incomeValue = new ArrayList<>();
+    ArrayList<Integer> incomeFrequency = new ArrayList<>();
+    ArrayList<String> incomeSource = new ArrayList<>();
+    CryptFile encryptedFile = new CryptFile();
+    int j = 0;
+    int i;
+
     void fullFileReader() {
-        try {
-            fis = new FileInputStream(income);
-
-            // Here BufferedInputStream is added for fast reading.
-            bis = new BufferedInputStream(fis);
-            dis = new DataInputStream(bis);
-
-            // dis.available() returns 0 if the file does not have more lines.
-            int i = 0;
-            while (dis.available() != 0) {
-                String line = dis.readLine();
-                int j = 0;
-                /*
-                encryptedStringBuilder.append(line);
-                     
-                String encryptedFile = encryptedStringBuilder.toString();
-                */
-                for (String s : line.split("\\s+")) {
-                    
-                    
-                    if (j == 0) {
-                        incomeSource.set(i, s);
-                        System.out.println(incomeSource.get(i));
-                    } else if (j == 1) {
-                        incomeValue.set(i, Double.parseDouble(s));
-                        System.out.println(incomeSource.get(i));
-                    }else if (j == 2){
-                        incomeFrequency.set(i, Integer.parseInt(s));
-                        System.out.println(incomeFrequency.get(i));
-                    } else {
-                        System.out.println("file error");
-                    }
-                    j++;
-                }
-                i++;
-                System.out.println();
-            }
-
-            // dispose all the resources after using them.
-            fis.close();
-            bis.close();
-            dis.close();
-
-            System.out.println("file read succesfully.");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            System.out.println("file not found.");
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("errorrororor.");
-        }catch (NumberFormatException e){
-            e.printStackTrace();
-            System.out.println("errorrororor. number read");
+        String string = encryptedFile.readEncryptedString("incomes.txt");
+        if (string == null) {
+            return;
         }
+        for (String a : string.split("\\.")) {
+            for (String s : a.split("\\s+")) {
+
+                if (j == 0) {
+                    incomeSource.set(i, s);
+                    System.out.println(incomeSource.get(i));
+                } else if (j == 1) {
+                    incomeValue.set(i, Double.parseDouble(s));
+                    System.out.println(incomeSource.get(i));
+                } else if (j == 2) {
+                    incomeFrequency.set(i, Integer.parseInt(s));
+                    System.out.println(incomeFrequency.get(i));
+                } else {
+                    System.out.println("file error");
+                }
+                j++;
+            }
+        }
+
     }
 }

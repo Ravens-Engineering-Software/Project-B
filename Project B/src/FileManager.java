2aaceb32443e183a462d4ea67712a66c2029e484
Jacob Larose <stonejake1205@gmail.com>
2015-06-12 10:50:17 -0400

    fixed merge i think

---
+++ Project B/src/FileManager.java
@@ -0,0 +1,65 @@
+
+import java.util.ArrayList;
+import javax.swing.table.DefaultTableModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Jacob Larose
+ */
+public class FileManager {
+
+    ArrayList<Double> incomeValue = new ArrayList<>();
+    ArrayList<Integer> incomeFrequency = new ArrayList<>();
+    ArrayList<String> incomeSource = new ArrayList<>();
+    CryptFile encryptedFile = new CryptFile();
+    int j = 0;
+    int i;
+
+    void fullFileReader() {
+        String string = encryptedFile.readEncryptedString("incomes.txt");
+        if (string == null) {
+            return;
+        }
+        for (String a : string.split("\n")) {
+            for (String s : a.split("\\s+")) {
+
+                if (j == 0) {
+                    incomeSource.set(i, s);
+                    System.out.println(incomeSource.get(i));
+                } else if (j == 1) {
+                    incomeValue.set(i, Double.parseDouble(s));
+                    System.out.println(incomeSource.get(i));
+                } else if (j == 2) {
+                    incomeFrequency.set(i, Integer.parseInt(s));
+                    System.out.println(incomeFrequency.get(i));
+                } else {
+                    System.out.println("file error");
+                }
+                j++;
+            }
+            i++;
+        }
+
+    }
+    
+    
+    void fileWrite(DefaultTableModel model) {
+        StringBuilder data = new StringBuilder();
+        CryptFile cryptFile = new CryptFile();
+        
+        for (int row=0; row<model.getRowCount(); row++){
+            for (int column=0; column<model.getColumnCount(); column++){
+                data.append(model.getValueAt(row, column));
+                data.append(" ");
+            }
+            data.append("\n");
+        }
+        String unencryptedString = data.toString();
+        cryptFile.writeEncryptedString("incomes.txt", unencryptedString);
+    }
+}
